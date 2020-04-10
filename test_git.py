bda2943c8afc0b8128dd4eb2b246f17e561ff5c6
sunhh-ml <sun58454006@163.com>
2020-04-10 16:05:44 +0800

    Initial commit

---
+++ test_git.py
@@ -0,0 +1,9 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Time         : 2020/4/10 16:03
+# @Author       : Huanhuan sun
+# @Email        : sun58454006@163.com
+# @File         : test_git.py
+# @Software     : PyCharm
+# @Project      : ml
+
