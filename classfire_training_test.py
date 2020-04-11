a84ef513a3033820a691bc3124bbbd35c0f17ea0
sunhh-ml <sun58454006@163.com>
2020-04-11 23:51:13 +0800

    pytorch tutorials py program

--- classfire_training_test.py
+++ classfire_training_test.py
@@ -56,9 +56,9 @@ print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 class Net(nn.Module):  # nn.Module是所有神经网络的基类，我们自己定义任何神经网络，都要继承nn.Module! 即class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()  # 第二、三行都是python类继承的基本操作,此写法应该是python2.7的继承格式,但python3里写这个好像也可以
-        self.conv1 = nn.Conv2d(3, 6, 5)  # 添加第一个卷积层,调用了nn里面的Conv2d（）
+        self.conv1 = nn.Conv2d(3, 8, 5)  # 添加第一个卷积层,调用了nn里面的Conv2d，二维卷积方法，常用于处理图片，conv1d常用语处理文本
         self.pool = nn.MaxPool2d(2, 2)  # 最大池化层
-        self.conv2 = nn.Conv2d(6, 16, 5)  # 输入是6通道的图像，输出是16通道，也就是16个卷积核，卷积核是5*5
+        self.conv2 = nn.Conv2d(8, 16, 5)  # 输入是6通道的图像，输出是16通道，也就是16个卷积核，卷积核是5*5
         self.fc1 = nn.Linear(16 * 5 * 5, 120)  # 接着三个全连接层
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
